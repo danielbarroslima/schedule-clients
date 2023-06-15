bad9947c608dd904677b48dd4172489a14e6f695
Daniel Barros <daniel.barroslm@gmail.com>
2023-06-15 18:39:31 -0300

    specs DONE

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
+  validates :name, presence: true, length: { minimum: 2, maximum: 100 }
+  validates :email, presence: true, uniqueness: true,
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i }
+
   enum type_user: { disable: 0, normal_user: 5, admin_user: 9, enable: 16 }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,52 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe User do
+  before { user.valid? }
+
+  context 'create or update' do
+    let(:user)            { create(:user) }
+    let(:user_created)    { User.find_by(email: user.email) }
+    let(:user_attributes) { user_created.attribute_names }
+
+    it { expect(user_created.present?).to eq true }
+    it { expect(user_attributes).to eq %w[id email name active type_user created_at updated_at] }
+  end
+
+  context 'validations' do
+    context 'invalid email' do
+      let(:user) { build(:user, email: Faker::Name.name) }
+
+      it { expect(user.errors.full_messages).to  eq ['Email is invalid'] }
+    end
+
+    context 'invalid name' do
+      context 'too long' do
+        let(:user) { build(:user, name: Faker::Name.initials(number: 101)) }
+
+        it { expect(user.errors.full_messages).to eq ['Name is too long (maximum is 100 characters)'] }
+      end
+
+      context 'too short' do
+        let(:user) { build(:user, name: Faker::Name.initials(number: 1)) }
+
+        it { expect(user.errors.full_messages).to eq ['Name is too short (minimum is 2 characters)'] }
+      end
+    end
+
+    context 'email blank' do
+      let(:user) { build(:user, email: '') }
+
+      it { expect(user.errors.full_messages).to eq ["Email can't be blank", 'Email is invalid'] }
+    end
+
+    context 'name blank' do
+      let(:user) { build(:user, name: '') }
+
+      it do
+        expect(user.errors.full_messages).to eq ["Name can't be blank", 'Name is too short (minimum is 2 characters)']
+      end
+    end
+  end
 end
